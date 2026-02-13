af7c6d94d6c9c32506e2c29bad00587130c91c26
Mahesh <maheshawsj@gmail.com>
2026-02-13 17:10:33 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+
+
+
+echo "$PERSON1: Hey, how are you?"
+echo "$PERSON2: I'm good! How about you?"
+echo "$PERSON1: I'm doing great."
+echo "$PERSON2: What are you working on?"
+echo "$PERSON1: I'm learning Git and Bash scripting."
+echo "$PERSON2: That's awesome! Keep going!"
+echo "$PERSON1: Thank you!"
