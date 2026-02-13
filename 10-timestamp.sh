fcb9191ec4a26aa7273bd65e8a1d9fd5197d1754
Mahesh <maheshawsj@gmail.com>
2026-02-13 17:51:24 +0530

    time

---
+++ 10-timestamp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Start_time=$(date +%s)
+
+echo "Script executed at: $Start_time"
+
+sleep 5
+
+End_time=$(date +%s)
+
+echo "Script executed at: $End_time"
+
+Total_Time=$(($End_time-Start_time))
+
+echo "Script executed at" $Total_Time"
