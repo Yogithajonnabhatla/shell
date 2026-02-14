961b565cf9fcdf7c4b8778afa31e4b6f521afe06
Mahesh <maheshawsj@gmail.com>
2026-02-14 16:05:23 +0530

    pwd

--- 11-specialvarbiales.sh
+++ 11-specialvarbiales.sh
@@ -9,3 +9,36 @@ fi
 echo "Installing Nginx"
 dnf install nginx -y
 
+
+if [ $? -ne 0 ]; then
+   echo "Installing nginx ...Failure"
+
+   else
+
+   echo "Installing nginx...Success"
+fi
+
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+   echo "Installing mysql ...Failure"
+
+   else
+
+   echo "Installing mysql...Success"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then 
+ 
+   echo "Installing nodejs...Failure"
+
+else
+   echo "Installing nidejs...Success"
+fi      
+
+
+
+
